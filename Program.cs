e7f13106a807d1481eb22b6c981999d93c6ebdbc
Silent <silent3_3@mail.ru>
2023-04-25 16:52:24 +0300

    Решение задачи №62

--- Program.cs
+++ Program.cs
@@ -8,23 +8,42 @@
 int[,] array = FillArray(4, 4);
 PrintArray(array);
 
-
-
-int[,] FillArray(int row, int col)
+int[,] FillArray(int row, int col) // Спирально заполняет массив 4х4 последовательностью чисел от 1 до 16
 {
     int[,] array = new int[row, col];
     int counter = 1;
     for (int i = 0; i < row; i++)
     {
         array[0, i] = counter;
-        counter ++;
+        counter++;
     }
-    for (int i = 0; i < col; i++)
+    for (int i = 1; i < col; i++)
     {
-        array[i+1, col-1] = counter;
-        counter ++;
+        array[i, col - 1] = counter;
+        counter++;
+    }
+    for (int i = 2; i >= 0; i--)
+    {
+        array[row - 1, i] = counter;
+        counter++;
+    }
+    for (int i = 2; i > 0; i--)
+    {
+        array[i, 0] = counter;
+        counter++;
+    }
+    for (int i = 1; i < row - 1; i++)
+    {
+        array[1, i] = counter;
+        counter++;
+    }
+    for (int i = 2; i >= 1; i--)
+    {
+        array[2, i] = counter;
+        counter++;
     }
     return array;
+
 }
 
 void PrintArray(int[,] array) // Печатает двумерный массив
