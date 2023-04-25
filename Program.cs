a11e4cdd219394e7b8763a2233463ecce11e3ffe
Silent <silent3_3@mail.ru>
2023-04-25 16:21:48 +0300

    условие задачи №62

--- Program.cs
+++ Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 62. Напишите программу, которая заполнит спирально массив 4 на 4.
+// Например, на выходе получается вот такой массив:
+// 01 02 03 04
+// 12 13 14 05
+// 11 16 15 06
+// 10 09 08 07
+
+
+int[,] FillArray(int row, int col)
+{
+    int[,] array = new int[row, col];
+    int counter = 1;
+    for (int i = 0; i < row; i++)
+    {
+        array[0, i] = counter;
+        counter ++;
+    }
+    for (int i = 0; i < col; i++)
+    {
+        array[i+1, col-1] = counter;
+        counter ++;
+    }
+
+}
