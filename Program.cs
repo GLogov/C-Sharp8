52d3d76e1a55ebbf5db11e78613115bcc3e86330
Silent <silent3_3@mail.ru>
2023-04-25 17:04:53 +0300

    Решение задачи №60

--- Program.cs
+++ Program.cs
@@ -6,3 +6,42 @@
 // 26(1,0,1) 55(1,1,1)
 
 
+int[,,] array = Generate3DArray(2, 2, 2);
+PrintArray(array);
+
+int[,,] Generate3DArray(int m, int n, int l) // Создает трехмерный массив
+{
+    int[,,] array = new int[m, n, l];
+
+    Random random = new Random();
+    int counter = 10;
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            for (int k = 0; k < l; k++)
+            {
+                array[i, j, k] = counter;
+                counter += 3;
+            }
+
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,,] array) // Печатает трехмерный массив
+{
+    for (var i = 0; i < array.GetLength(0); i++)
+    {
+        for (var j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                Console.Write($"<{array[i, j, k]}> ({i}, {j}, {k})");
+                Console.Write(" ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
